b3bce5b602acb5b529c2126c18bc20b0cff84e4c
Amir <mfathulqorib97@gmail.com>
2025-05-24 11:09:55 +0700

    fix: supresshydration warnin, login url, dashboard fetch data

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Add this line:
-  suppressHydrationWarning: true,
   /* config options here */
 };
 
