625154c3bff6915b1856a19efdfc02490c94aee4
Amir <mfathulqorib97@gmail.com>
2025-05-24 11:12:38 +0700

    update login page

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -9,8 +9,6 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true}>{children}</body>
-    </html>
+    <body suppressHydrationWarning={true}>{children}</body>
   )
 }
