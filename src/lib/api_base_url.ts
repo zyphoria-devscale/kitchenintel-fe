9deed807ae02779488a1d51a843ef35fceab644c
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-05-25 15:26:25 +0700

    fix: websocket url

--- src/lib/api_base_url.ts
+++ src/lib/api_base_url.ts
@@ -1,1 +1,2 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000/api';
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://127.0.0.1:8000';
